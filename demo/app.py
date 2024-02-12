217ed9fcbe0e1fdf7da767936c3cb35f1042e13d
clement-pages <pagesclement31@gmail.com>
2024-02-12 15:35:34 +0100

    app: add a dropdown component to select `pyannote` pipeline

--- demo/app.py
+++ demo/app.py
@@ -1,27 +1,60 @@
 import os
-import gradio as gr
 from pathlib import Path
-from gradio_annotatedaudio import AnnotatedAudio
+from typing import List
 
+import gradio as gr
+from gradio_annotatedaudio import AnnotatedAudio
 from pyannote.audio import Pipeline
 
 example = AnnotatedAudio().example_inputs()
+annotated_audio = AnnotatedAudio(type="filepath", interactive=True)
 
-def get_diarization(filepath: str):
-    filepath = Path(filepath)
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=os.environ["HG_TOKEN"])
+
+def get_available_pipelines() -> List[str]:
+    """ Get pipelines list, sorted according to the
+    order in which they were last modified"""
+    from huggingface_hub import HfApi
+
+    available_pipelines = [
+        p.modelId for p in HfApi().list_models(filter="pyannote-audio-pipeline", sort="last_modified", direction=-1)
+    ]
+    return list(filter(lambda p: p.startswith("pyannote/"), available_pipelines))
+
+
+def apply_pipeline(pipeline_name: str, filepath: str):
+    """ Apply specified pipeline on the indicated file"""
+    pipeline = Pipeline.from_pretrained(
+        pipeline_name, use_auth_token=os.environ["HG_TOKEN"]
+    )
     annotations = pipeline(filepath)
 
     return (filepath, annotations)
 
-annotated_audio = AnnotatedAudio(type="filepath", interactive=True)
 
-demo = gr.Interface(
-    get_diarization,
-    inputs=annotated_audio,
-    outputs=annotated_audio,
-    # examples=[[example]],  # uncomment this line to view the "example version" of your component
-)
+with gr.Blocks() as demo:
+    gr.Markdown(
+        "Welcome to the [pyannote.audio](https://github.com/pyannote/pyannote-audio) app !"
+    )
+
+    pipelines = get_available_pipelines()
+    pipelines_choice = gr.Dropdown(
+        choices=pipelines,
+        label="Choose one pipeline to apply:",
+        value=pipelines[0],
+        interactive=True,
+    )
+
+    annotated_audio = AnnotatedAudio(
+        type="filepath",
+        interactive=True,
+    )
+
+    run_btn = gr.Button("Run pipeline")
+    run_btn.click(
+        fn=apply_pipeline,
+        inputs=[pipelines_choice, annotated_audio],
+        outputs=annotated_audio,
+    )
 
 
 if __name__ == "__main__":
