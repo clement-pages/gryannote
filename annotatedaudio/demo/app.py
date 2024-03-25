db250afebe4f7cee1b029f7221fd3d2658520aeb
clement-pages <pagesclement31@gmail.com>
2024-03-25 15:29:20 +0100

    update(annotatedaudio): use `pipelineselector` component in the demo

--- annotatedaudio/demo/app.py
+++ annotatedaudio/demo/app.py
@@ -3,29 +3,17 @@ from typing import List
 
 import gradio as gr
 from gradio_annotatedaudio import AnnotatedAudio
+from gradio_pipelineselector import PipelineSelector
 from pyannote.audio import Pipeline
 
 example = AnnotatedAudio().example_inputs()
+
 annotated_audio = AnnotatedAudio(type="filepath", interactive=True)
 
 
-def get_available_pipelines() -> List[str]:
-    """ Get pipelines list, sorted according to the
-    order in which they were last modified"""
-    from huggingface_hub import HfApi
-
-    available_pipelines = [
-        p.modelId for p in HfApi().list_models(filter="pyannote-audio-pipeline", sort="last_modified", direction=-1)
-    ]
-    return list(filter(lambda p: p.startswith("pyannote/"), available_pipelines))
-
-
-def apply_pipeline(pipeline_name: str, data: AnnotatedAudio):
+def apply_pipeline(pipeline: Pipeline, data: AnnotatedAudio):
     """ Apply specified pipeline on the indicated file"""
     filepath, _ = data
-    pipeline = Pipeline.from_pretrained(
-        pipeline_name, use_auth_token=os.environ["HG_TOKEN"]
-    )
     annotations = pipeline(filepath)
 
     return (filepath, annotations)
@@ -39,14 +27,7 @@ with gr.Blocks() as demo:
     gr.Markdown(
         "Welcome to the [pyannote.audio](https://github.com/pyannote/pyannote-audio) app !"
     )
-    pipelines = get_available_pipelines()
-    pipelines_choice = gr.Dropdown(
-        choices=pipelines,
-        label="Choose one pipeline to apply:",
-        value=pipelines[0],
-        interactive=True,
-    )
-
+    pipeline_selector = PipelineSelector(source="dropdown")
     annotated_audio = AnnotatedAudio(
         type="filepath",
         interactive=True,
@@ -56,7 +37,7 @@ with gr.Blocks() as demo:
     run_btn = gr.Button("Run pipeline")
     run_btn.click(
         fn=apply_pipeline,
-        inputs=[pipelines_choice, annotated_audio],
+        inputs=[pipeline_selector, annotated_audio],
         outputs=annotated_audio,
     )
 
