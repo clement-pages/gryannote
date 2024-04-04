e71552858018426bbbebc40041920f182e90ced4
clement-pages <pagesclement31@gmail.com>
2024-04-04 10:11:01 +0200

    update(pipelineselector): bump to 0.1.0

--- pipelineselector/demo/app.py
+++ pipelineselector/demo/app.py
@@ -1,14 +1,5 @@
-import os
-
 import gradio as gr
 from gradio_pipelineselector import PipelineSelector
-from pyannote.audio import Pipeline
-
-example = PipelineSelector().example_inputs()
-
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1", use_auth_token=os.environ["HG_TOKEN"]
-)
 
 with gr.Blocks() as demo:
     pipeline_selector = PipelineSelector()
