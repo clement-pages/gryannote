fb4e39d2de75c1efe51bb3154fe29e5591d9a81a
Clément Pagés <55240756+clement-pages@users.noreply.github.com>
2024-04-19 16:19:17 +0200

    demo(annotatedaudio): update component demo (#12)

--- annotatedaudio/demo/app.py
+++ annotatedaudio/demo/app.py
@@ -3,61 +3,14 @@ from gradio_annotatedaudio import AnnotatedAudio
 from gradio_pipelineselector import PipelineSelector
 from pyannote.audio import Pipeline
 
-example = AnnotatedAudio().example_inputs()
-
 annotated_audio = AnnotatedAudio(type="filepath", interactive=True)
 
 
-def get_annotations():
-    audio = ...
+def apply_pipeline(audio):
     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1")
     annotations = pipeline(audio)
     return (audio, annotations)
 
+demo = gr.Interface(apply_pipeline, inputs=annotated_audio, outputs=annotated_audio)
 
-value = get_annotations()
-
-
-def apply_pipeline(pipeline: Pipeline, audio):
-    """Apply specified pipeline on the indicated audio file"""
-    annotations = pipeline(audio)
-
-    return (audio, annotations)
-
-
-with gr.Blocks() as demo:
-    gr.Markdown(
-        "Welcome to the [pyannote.audio](https://github.com/pyannote/pyannote-audio) app !"
-    )
-    pipeline_selector = PipelineSelector()
-    pipeline_selector.select(
-        fn=pipeline_selector.on_select,
-        inputs=pipeline_selector,
-        outputs=pipeline_selector,
-        preprocess=False,
-        postprocess=False,
-    )
-    pipeline_selector.change(
-        fn=pipeline_selector.on_change,
-        inputs=pipeline_selector,
-        outputs=pipeline_selector,
-        preprocess=False,
-        postprocess=False,
-    )
-
-    annotated_audio = AnnotatedAudio(
-        type="filepath",
-        # value=value,
-        interactive=True,
-    )
-
-    run_btn = gr.Button("Run pipeline")
-    run_btn.click(
-        fn=apply_pipeline,
-        inputs=[pipeline_selector, annotated_audio],
-        outputs=annotated_audio,
-    )
-
-
-if __name__ == "__main__":
-    demo.launch()
+demo.launch()
