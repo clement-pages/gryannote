6a495c50a232375bbeca10ca5f421a7971ac4183
clement-pages <pagesclement31@gmail.com>
2024-06-20 14:31:12 +0200

    update dialog box CSS

--- annotatedaudio/frontend/shared/Dialog.svelte
+++ annotatedaudio/frontend/shared/Dialog.svelte
@@ -17,12 +17,14 @@
     export function openDialog(labelID: string): void {
         dialog.showModal();
         shortcut = labelID;
+        document.getElementById("dialog-box").hidden = false;
         isOpen = true;
     }
 
     function onClose(): void {
         dialog.close();
         name = color = shortcut = undefined;
+        document.getElementById("dialog-box").hidden = true;
         isOpen = false;
     }
 
@@ -43,9 +45,85 @@
 
 </script>
 
-<dialog bind:this={dialog}>
+<dialog bind:this={dialog} class="dialog-box" id="dialog-box" hidden>
     <h2> {title} </h2>
-    <input type="text" bind:value={name}>
-    <button on:click={onSubmit}>Submit</button>
-    <button on:click={onClose}>Close</button>
+    <input type="text" maxlength="30" bind:value={name}>
+    <div class="dialog-buttons">
+        <button on:click={onClose} id="cancel" >Cancel</button>
+        <button on:click={onSubmit} id="submit">Submit</button>
+    </div>
 </dialog>
+
+<style>
+    .dialog-box[hidden] {
+        display: none !important;
+    }
+
+    .dialog-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background: var(--block-background-fill);
+
+        border-color: var(--block-border-color);
+        border-radius: var(--block-radius);
+        border-width: var(--block-border-width);
+        box-shadow: var(--block-shadow);
+
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .dialog-box::backdrop {
+        background: rgba(0, 0, 0, 0.3);
+    }
+
+    h2 {
+        color: var(--button-secondary-text-color);
+    }
+
+    input {
+        border: 1px solid;
+        border-color: var(--block-border-color);
+        border-radius: var(--block-radius);
+        color: var(--button-secondary-text-color);
+        margin: 0.5em;
+    }
+
+    .dialog-buttons {
+        display: flex;
+        flex-direction: row;
+    }
+
+    button {
+        border: var(--button-border-width) solid;
+        border-radius: var(--button-large-radius);
+        font-weight: var(--button-large-text-weight);
+        padding: var(--button-large-padding);
+        margin: 1em;
+    }
+
+    #cancel {
+        border-color: var(--button-secondary-border-color);
+        color: var(--button-secondary-text-color);
+        background: var(--button-secondary-background-fill);
+    }
+
+    #cancel:hover {
+        border-color: var(--button-secondary-border-color-hover);
+        color: var(--button-secondary-text-color-hover);
+        background: var(--button-secondary-background-fill-hover);
+    }
+
+    #submit {
+        border-color: var(--button-primary-border-color);
+        color: var(--button-primary-text-color);
+        background: var(--button-primary-background-fill);
+    }
+
+    #submit:hover {
+        border-color: var(--button-primary-border-color-hover);
+        color: var(--button-primary-text-color-hover);
+        background: var(--button-primary-background-fill-hover);
+    }
+</style>
